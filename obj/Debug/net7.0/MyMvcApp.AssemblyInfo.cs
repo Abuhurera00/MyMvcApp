9ca3d1beacb8734cfc32bed180a751f7ec916ec7
Abuhurera00 <hurerag24@gmail.com>
2025-08-02 12:32:16 +0500

    downgrade version

--- obj/Debug/net7.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net7.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02d74818c56d6305ddac748f956d51e2d5859cf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+470c7e62c05cb040f41c4d71cb573474e573a831")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
